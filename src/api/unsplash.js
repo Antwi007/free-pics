40cba654a6fa72cccdad158ff9fa8ae9ed0891cd
keremguventurk3123 <kerem.guventurk2@gmail.com>
2020-09-15 21:39:09 +0300

    broke google rules

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID -t5CokEJ00-R4mqoCBKYl3ztEO0nxhOZxdHAtKaqVvE'
-    }
-})
